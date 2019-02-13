95ed652f352984b10cd45cd745eb72e2e192d942
Don Le <donle@wlan-t-180-231-dhcp.CSUChico.EDU>
2019-02-13 11:43:59 -0800

    Edited main.cpp to use Vec

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
+    vector<string> theVec; //array of 5 strings
+    int numItems = 0;
+    char input;
+    string items;
+    do{
+        cout<<"\n==GROCERY LIST MANAGER==";
+        cout<<"\nEnter your choice: ";
+        cout<<"\n (A)dd an item";
+        cout<<"\n (Q)uit";
+        cout<<"\nYour choice (A/Q): ";
+        cin>>input;
+        if(input == 'A'){
+            cin>>items;
+        numItems ++;
+        theVec.push_back(items);
+        }
+    }
+    while(input != 'Q' );
+    for(int i = 0; i < theVec.size(); i++)
+    {
+        cout << theVec[i] << endl;
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-
-  return 0;
+    }
+    return 0;
 }
